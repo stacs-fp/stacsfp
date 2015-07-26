c731c89cac65e18ba999c7ec61275b7b1496b454
Christopher Schwaab <christopher.schwaab@gmail.com>
2015-07-26 19:58:13 +0100

    Add people, fixup navbar, and start recent posts.

--- site.hs
+++ site.hs
@@ -1,69 +1,85 @@
 --------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
-import           Data.Monoid (mappend)
-import           Hakyll
+import Data.Maybe
+import Data.Monoid (mappend)
+import qualified Data.Map as M
+import Hakyll
+import System.FilePath (takeBaseName)
 
+copyAll = route idRoute >> compile copyFileCompiler
+copyAllIn path = match path copyAll
 
 --------------------------------------------------------------------------------
 main :: IO ()
 main = hakyll $ do
     -- FIXME there's surely a better way of doing this...
-    match "js/*" (route idRoute >> compile copyFileCompiler)
-    match "lib/**/*" (route idRoute >> compile copyFileCompiler)
-    match "images/*" (route idRoute >> compile copyFileCompiler)
-    match "images/**/*" (route idRoute >> compile copyFileCompiler)
+    mapM_ copyAllIn
+      ["js/*"
+      ,"lib/**/*"
+      ,"images/*"
+      ,"images/**/*"]
 
     match "css/*" $ do
         route   idRoute
         compile compressCssCompiler
 
-    match (fromList ["about.rst", "contact.markdown"]) $ do
-        route   $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/default.html" defaultContext
-            >>= relativizeUrls
+    --match (fromList ["about.rst", "contact.markdown"]) $ do
+    --    route   $ setExtension "html"
+    --    compile $ pandocCompiler
+    --        >>= loadAndApplyTemplate "templates/default.html" defaultContext
+    --        >>= relativizeUrls
 
     match "posts/*" $ do
         route $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/post.html"    postCtx
-            >>= loadAndApplyTemplate "templates/default.html" postCtx
-            >>= relativizeUrls
+        compile $
+            pandocCompiler
+              >>= loadAndApplyTemplate "templates/post.html"    standardContext
+              >>= loadAndApplyTemplate "templates/default.html" standardContext
+              >>= relativizeUrls
 
     create ["archive.html"] $ do
         route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let archiveCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Archives"            `mappend`
-                    defaultContext
-
+        compile $
             makeItem ""
-                >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
-                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
+                >>= loadAndApplyTemplate "templates/archive.html" standardContext
+                >>= loadAndApplyTemplate "templates/default.html" standardContext
                 >>= relativizeUrls
 
 
-    match "index.html" $ do
+    match "*.html" $ do
         route idRoute
         compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let indexCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Home"                `mappend`
-                    defaultContext
-
             getResourceBody
-                >>= applyAsTemplate indexCtx
-                >>= loadAndApplyTemplate "templates/default.html" indexCtx
+                >>= applyAsTemplate standardContext
+                >>= loadAndApplyTemplate "templates/default.html" standardContext
                 >>= relativizeUrls
 
     match "templates/*" $ compile templateCompiler
 
-
 --------------------------------------------------------------------------------
-postCtx :: Context String
-postCtx =
-    dateField "date" "%B %e, %Y" `mappend`
-    defaultContext
+standardContext :: Context String
+standardContext =
+  overrideTitle "title" `mappend`
+  titleField "active" `mappend`
+  dateField "date" "%B %e, %Y" `mappend`
+  postsField `mappend`
+  defaultContext
+
+postsField :: Context String
+postsField =
+  listField
+    "posts"
+    (mconcat [field "url" $ return . const "url0"
+             ,field "title" $ return . const "title0"
+             ,field "date" $ return . const "date0"])
+    (fmap sequence (makeItem ["A", "B"]))
+  --  --(recentFirst =<< loadAll ("posts/*" .&&. complement )
+  --  --   :: Compiler [Item String])
+
+overrideTitle :: String -> Context String
+overrideTitle fld = do
+  field fld $ \item -> do
+   let itemId = itemIdentifier item
+   let defaultTitle = takeBaseName (toFilePath itemId)
+   title <- getMetadata itemId
+   return $ fromMaybe defaultTitle (M.lookup "title" title)
