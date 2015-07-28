03d20c88aa7f011dab5a49480ee63db6c2c404d0
Christopher Schwaab <christopher.schwaab@gmail.com>
2015-07-28 23:14:46 +0100

    Add publication list (scraped scholar), generate per-paper bibtex files, remove unused code.

--- site.hs
+++ site.hs
@@ -1,58 +1,55 @@
 --------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
+import Control.Applicative
+import Control.Monad (unless)
+import Data.Foldable (for_)
 import Data.Maybe
 import Data.Monoid (mappend)
 import qualified Data.Map as M
 import Hakyll
-import System.FilePath (takeBaseName)
-
-copyAll = route idRoute >> compile copyFileCompiler
-copyAllIn path = match path copyAll
+import qualified Text.BibTeX.Entry as E
+import qualified Text.BibTeX.Format as F
+import Text.BibTeX.Parse (file, skippingLeadingSpace )
+import Text.Parsec.String (parseFromFile)
+import System.Directory (createDirectoryIfMissing, doesFileExist, getDirectoryContents)
+import System.FilePath ((</>), (<.>), takeBaseName, takeExtension)
+import System.IO (hPutStrLn, stderr)
 
 --------------------------------------------------------------------------------
 main :: IO ()
 main = hakyll $ do
-    -- FIXME there's surely a better way of doing this...
-    mapM_ copyAllIn
-      ["js/*"
-      ,"lib/**/*"
-      ,"images/*"
-      ,"images/**/*"]
+    preprocess $ do
+      fs <- filter ((== ".bib") . takeExtension) <$> getDirectoryContents "publications"
+      createDirectoryIfMissing True "bibtex"
+      for_ fs $ \f -> do
+        r <- parseFromFile (skippingLeadingSpace file) ("publications" </> f)
+        case r of
+          Left err ->
+            hPutStrLn stderr ("Error processing file '" ++ f ++ "' (skipping): " ++ show err)
+          Right bs ->
+            for_ bs $ \b -> do
+              let bibFile = "bibtex" </> E.identifier b <.> "bib"
+              p <- doesFileExist bibFile
+              unless p (writeFile bibFile (F.entry b))
+
+    match ("js/*" .||.
+           "lib/**/*" .||.
+           "images/**" .||.
+           "bibtex/*") $ do
+        route idRoute
+        compile copyFileCompiler
 
     match "css/*" $ do
         route   idRoute
         compile compressCssCompiler
 
-    --match (fromList ["about.rst", "contact.markdown"]) $ do
-    --    route   $ setExtension "html"
-    --    compile $ pandocCompiler
-    --        >>= loadAndApplyTemplate "templates/default.html" defaultContext
-    --        >>= relativizeUrls
-
-    match "posts/*" $ do
-        route $ setExtension "html"
-        compile $
-            pandocCompiler
-              >>= loadAndApplyTemplate "templates/post.html"    standardContext
-              >>= loadAndApplyTemplate "templates/default.html" standardContext
-              >>= relativizeUrls
-
-    create ["archive.html"] $ do
-        route idRoute
-        compile $
-            makeItem ""
-                >>= loadAndApplyTemplate "templates/archive.html" standardContext
-                >>= loadAndApplyTemplate "templates/default.html" standardContext
-                >>= relativizeUrls
-
-
     match "*.html" $ do
         route idRoute
-        compile $ do
-            getResourceBody
-                >>= applyAsTemplate standardContext
-                >>= loadAndApplyTemplate "templates/default.html" standardContext
-                >>= relativizeUrls
+        compile $
+            getResourceBody >>=
+            applyAsTemplate standardContext >>=
+            loadAndApplyTemplate "templates/default.html" standardContext >>=
+            relativizeUrls
 
     match "templates/*" $ compile templateCompiler
 
